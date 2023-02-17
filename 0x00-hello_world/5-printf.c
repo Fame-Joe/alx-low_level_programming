bb8d4f024a4c321f284e3b84f71bb1d0d9c05be9
Fame-Joe <phame233@gmail.com>
2023-02-17 09:52:36 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints "with proper grammar, but the outcome is
  * a piece of art," followerd by new line.
  * Return: Always 0.
- *
  */
 
 int main(void)
